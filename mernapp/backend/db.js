796faca532e1bdad0f58a90f29ed480e936df5ae
Maryam <maryamjaved.xo@gmail.com>
2024-02-21 23:41:29 +0500

    add user

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://maryamjavedshk:lorelai@cluster0.0yga0wa.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB = () => {
-  mongoose.connect(mongoURI, { useNewUrlParser: true })
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
+const mongoURI = 'mongodb+srv://maryamjavedshk:lorelai@cluster0.0yga0wa.mongodb.net/gofoodmern';
 
-    .catch((error) => {
-      console.error('Error connecting to MongoDB:', error);
-    })
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    console.log('Connected to MongoDB');
 
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    // console.log(data);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
+
 module.exports = mongoDB;
+
+
 
