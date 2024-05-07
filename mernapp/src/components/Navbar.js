138a42d0527fca88418f0574591f612bac720894
Maryam <maryamjaved.xo@gmail.com>
2024-05-07 11:42:58 +0500

    cart working

--- mernapp/src/components/Navbar.js
+++ mernapp/src/components/Navbar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge'
+import Modal from '../Modal';
+import Cart from '../screens/Cart';
 
 export default function Navbar() {
+  const [cartView, setCartView] = useState(false)
   const navigate = useNavigate()
   const handleLogout = () => {
     localStorage.removeItem("authToken")
@@ -39,7 +43,14 @@ export default function Navbar() {
             ) :
               <>
                 <div className='ms-auto'>
-                  <div className='btn bg-white text-success mx-1'>My Cart</div>
+                  <div className='btn bg-white text-success mx-1' onClick={() => { setCartView(true) }}>My Cart {" "}
+                    <Badge pill bg="danger">2</Badge>
+                  </div>
+                  {cartView ?
+                    <Modal onClose={() => { setCartView(false) }}>
+                      <Cart></Cart>
+                    </Modal>
+                    : null}
                   <div className='btn bg-white text-danger mx-1' onClick={handleLogout}>Logout</div>
                 </div>
 
